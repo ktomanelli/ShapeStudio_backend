8f09b6d646c7cfa36d43e13c99f5fefa89efe262
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-07-23 19:27:03 -0400

    added a load_new route/method which returns a blank scene

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,11 +1,15 @@
 class ScenesController < ApplicationController
     def load
         @user = User.first
-        render json: Scene.first
+        render json: {scene:Scene.second,camera:Camera.first}
     end
-
+    def load_new        
+        @user = User.first
+        render json: {scene:Scene.first,camera:Camera.first}
+    end
     def save
         @user = User.first
-        Scene.create(user_id:@user.id,scene_string:params[:scene])
+        scene = Scene.create(user_id:@user.id,scene_string:params[:scene])
+        Camera.create(scene_id:scene.id,camera_string:params[:camera])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/scene/save', to: 'scenes#save'
   get '/scene/load', to: 'scenes#load'
+  get '/scene/loadnew', to: 'scenes#load_new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
