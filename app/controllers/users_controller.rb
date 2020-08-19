acb303aca4446068518e628b408f613d2c7879a7
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-08-19 17:22:05 -0400

    able to sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     end
 
     def login
-        @user = User.find_by(username:params[:username])
+        @user = User.find_by(email:params[:email])
         if @user && @user.authenticate(params[:password])
             @token = encode_token({user_id: @user.id})
             render json: { user: UserSerializer.new(@user), token: @token }
