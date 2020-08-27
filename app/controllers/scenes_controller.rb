162eda31baa41f58a875cb96cff06c77dc43650f
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-08-27 18:28:08 -0400

    fixed auth and added env var for auth secret

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,32 +1,26 @@
 class ScenesController < ApplicationController
+    before_action :authorized
+
     def load
         @user = User.first
         @scene = Scene.find(params[:id])
         render json: {scene:@scene}
     end
-    def load_new        
-        @user = User.first
-        render json: {scene:Scene.first,camera:Camera.first}
-    end
+
     def save
-        @user = User.first
         Scene.create(user_id:@user.id,scene_string:params[:scene],save_name:params[:save_name])
     end
+
     def index
-        @user = User.first
         @scenes = @user.scenes
         render json: @scenes
     end
+
     def update
-        @user = User.first
         @scene = Scene.find(params[:id])
         @scene.update(:scene_string=>params[:scene_string])
     end
 
-    # private
-    # def scene_params
-    #   params.require(:scene).permit(:scene_string)
-    # end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  post '/scenes/save', to: 'scenes#save'
   get '/scenes/load/:id', to: 'scenes#load'
   get '/scenes', to: 'scenes#index'
-  patch '/scenes/:id', to: 'scenes#update'
-  post '/users', to: 'users#create'
   get '/users/stay_logged_in', to: 'users#stay_logged_in'
-  post '/users/login', to: 'users#login'
 
+  post '/users', to: 'users#create'
+  post '/scenes/save', to: 'scenes#save'
+  post '/users/login', to: 'users#login'
+  
+  patch '/scenes/:id', to: 'scenes#update'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
