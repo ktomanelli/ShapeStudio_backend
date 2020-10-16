be6a76d63637ff258961e34a7de9c1a74a33f0f7
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-10-16 15:54:41 -0400

    active storage now working

--- app/serializers/scene_serializer.rb
+++ app/serializers/scene_serializer.rb
@@ -1,3 +1,17 @@
 class SceneSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :scene_string, :save_name
+  include Rails.application.routes.url_helpers
+  attributes :id, :user_id, :scene_string, :save_name,:screenshot
+  belongs_to :user
+  def screenshot
+    if object.screenshot.attached?
+      rails_blob_path(object.screenshot, only_path: true)
+    end
+  end
+  # def screenshot
+  #   if object.screenshot.attached?
+  #     {
+  #       url: rails_blob_url(object.screenshot)
+  #     }
+  #   end
+  # end
 end
